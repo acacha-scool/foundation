4e32712e8bfff4f1aa76586bf45f4c3216ab0aa3
Sergi Tur Badenas <sergiturbadenas@gmail.com>
2017-09-07 14:08:11 +0200

    Addes helpers to seed locations

---
+++ src/helpers.php
@@ -0,0 +1,40 @@
+<?php
+
+use Scool\Foundation\Location;
+
+if (! function_exists('seed_locations')) {
+    /**
+     * Seed locations.
+     */
+    function seed_locations()
+    {
+        first_or_create_location('20.2','Aula 20.2');
+        first_or_create_location('20.3','Aula 20.3');
+        first_or_create_location('20.4','Aula 20.4');
+        //TODO
+    }
+}
+
+if (! function_exists('first_or_create_location')) {
+    /**
+     * Create location if not exists and return new o already existing location.
+     *
+     * @param $code
+     * @param $name
+     * @return mixed
+     */
+    function first_or_create_location($code, $name)
+    {
+        try {
+            $law = Location::create([
+                'code' => $code,
+                'name' => $name,
+            ]);
+            return $law;
+        } catch (Illuminate\Database\QueryException $e) {
+            return Location::where([
+                ['code', '=', $code]
+            ]);
+        }
+    }
+}
